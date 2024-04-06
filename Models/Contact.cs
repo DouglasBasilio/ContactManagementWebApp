dcbad1b699ed1a551a5ca3874e71fe7ac979956c
Douglas Basilio <basilio.douglas@yahoo.com.br>
2024-04-06 12:45:04 -0300

    adjust views language and appsettings

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -8,15 +8,15 @@ namespace ContactManagementWebApp.Models
 
         [Required(ErrorMessage = "Name is required.")]
         [StringLength(255, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 255 characters.")]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Contact number is required.")]
         [RegularExpression(@"^\d{9}$", ErrorMessage = "Contact number must be 9 digits.")]
-        public string ContactNumber { get; set; }
+        public string ContactNumber { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email address is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
-        public string EmailAddress { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
 
         public bool IsDeleted { get; set; }
     }
