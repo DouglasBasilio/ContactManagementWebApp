a06ddf05c50bcd64a1d0103acf42f101a29472ba
Douglas Basilio <basilio.douglas@yahoo.com.br>
2024-04-12 13:47:53 -0300

    feat(master): ajuste nas controladoras

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Alfasoft.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginAsync(LoginViewModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Login == "alfasoft" && model.Password == "pwd123")
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, model.Login)
+                    };
+
+                    var userIdentity = new ClaimsIdentity(claims, "login");
+                    var principal = new ClaimsPrincipal(userIdentity);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Credenciais inválidas");
+                }
+            }
+
+            return View(model);
+        }
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using Alfasoft.Models;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 
 namespace Alfasoft.Controllers
 {
@@ -14,38 +13,63 @@ namespace Alfasoft.Controllers
             _dbContext = dbContext;
         }
 
+        private bool CustomerExists(int id)
+        {
+            return _dbContext.Customers.Any(e => e.ID == id);
+        }
+
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            var customers = _dbContext.Customers.ToList();
+            var customers = _dbContext.Customers.Where(c => !c.IsDeleted).ToList();
             return View(customers);
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
 
-        // GET: Contacts/Create
+        [AllowAnonymous]
+        [Route("/Home/Create")]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Contacts/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,ContactNumber,EmailAddress")] Customer contact)
+        [Route("/Home/Create")]
+        public async Task<IActionResult> Create([Bind("ID,Name,ContactNumber,EmailAddress")] Customer customer)
         {
             if (ModelState.IsValid)
             {
-                _dbContext.Add(contact);
+                _dbContext.Add(customer);
                 await _dbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(contact);
+            return View(customer);
         }
 
-        // GET: Contacts/Edit/5
+        [Route("/Home/Details")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _dbContext.Customers.FirstOrDefaultAsync(m => m.ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        [Route("/Home/Edit/{id}")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -53,20 +77,20 @@ namespace Alfasoft.Controllers
                 return NotFound();
             }
 
-            var contact = await _dbContext.Customers.FindAsync(id);
-            if (contact == null)
+            var customer = await _dbContext.Customers.FindAsync(id);
+            if (customer == null)
             {
                 return NotFound();
             }
-            return View(contact);
+            return View(customer);
         }
 
-        // POST: Contacts/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,ContactNumber,EmailAddress")] Customer contact)
+        [Route("/Home/Edit/{id}")]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,ContactNumber,EmailAddress")] Customer customer)
         {
-            if (id != contact.ID)
+            if (id != customer.ID)
             {
                 return NotFound();
             }
@@ -75,12 +99,12 @@ namespace Alfasoft.Controllers
             {
                 try
                 {
-                    _dbContext.Update(contact);
+                    _dbContext.Update(customer);
                     await _dbContext.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ContactExists(contact.ID))
+                    if (!CustomerExists(customer.ID))
                     {
                         return NotFound();
                     }
@@ -91,10 +115,10 @@ namespace Alfasoft.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(contact);
+            return View(customer);
         }
 
-        // GET: Contacts/Delete/5
+        [Route("/Home/Delete/{id}")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -102,39 +126,27 @@ namespace Alfasoft.Controllers
                 return NotFound();
             }
 
-            var contact = await _dbContext.Customers
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (contact == null)
+            var customer = await _dbContext.Customers.FirstOrDefaultAsync(m => m.ID == id);
+            if (customer == null)
             {
                 return NotFound();
             }
 
-            return View(contact);
+            return View(customer);
         }
 
-        // POST: Contacts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Route("/Home/Delete/{id}")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var contact = await _dbContext.Customers.FindAsync(id);
-            if (contact != null)
+            var customer = await _dbContext.Customers.FindAsync(id);
+            if (customer != null)
             {
-                _dbContext.Customers.Remove(contact);
+                customer.IsDeleted = true;
                 await _dbContext.SaveChangesAsync();
             }
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool ContactExists(int id)
-        {
-            return _dbContext.Customers.Any(e => e.ID == id);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
