f9420d012fc415e7f08bf7fc762414e17959f637
Douglas Basilio <basilio.douglas@yahoo.com.br>
2024-04-12 13:48:39 -0300

    feat(master): ajuste no custumer e validacao

--- Customer.cs
+++ Customer.cs
@@ -8,15 +8,17 @@ namespace Alfasoft
 
         [Required(ErrorMessage = "Name is required.")]
         [StringLength(255, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 255 characters.")]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Contact number is required.")]
         [RegularExpression(@"^\d{9}$", ErrorMessage = "Contact number must be 9 digits.")]
-        public string ContactNumber { get; set; } = string.Empty;
+        [UniqueContact(ErrorMessage = "Contact number must be unique.")]
+        public string ContactNumber { get; set; }
 
         [Required(ErrorMessage = "Email address is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
-        public string EmailAddress { get; set; } = string.Empty;
+        [UniqueEmail(ErrorMessage = "Email address must be unique.")]
+        public string EmailAddress { get; set; }
 
         public bool IsDeleted { get; set; }
     }
---
+++ Validation.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alfasoft
+{
+    public class UniqueContactAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var dbContext = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+
+            if (dbContext != null)
+            {
+                var customer = (Customer)validationContext.ObjectInstance;
+
+                // Verifica se já existe um contato com o mesmo número
+                if (dbContext.Customers.Any(c => c.ID != customer.ID && c.ContactNumber == (string)value))
+                {
+                    return new ValidationResult("Contact number must be unique.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+    public class UniqueEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var dbContext = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+
+            if (dbContext != null)
+            {
+                var customer = (Customer)validationContext.ObjectInstance;
+
+                // Verifica se já existe um contato com o mesmo endereço de e-mail
+                if (dbContext.Customers.Any(c => c.ID != customer.ID && c.EmailAddress == (string)value))
+                {
+                    return new ValidationResult("Email address must be unique.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
