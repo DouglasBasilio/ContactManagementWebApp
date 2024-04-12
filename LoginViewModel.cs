7d3cbe99d34a8ac138b7a3b68d1227146f86b669
Douglas Basilio <basilio.douglas@yahoo.com.br>
2024-04-12 13:49:38 -0300

    feat(master): criação classe login

---
+++ LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Alfasoft
+{
+    public class LoginViewModel
+    {
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+
+        public LoginViewModel()
+        {
+            Login = string.Empty;
+            Password = string.Empty;
+        }
+    }
+}
